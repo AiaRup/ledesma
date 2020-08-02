5760966b13e7e500fc895282a0c437a3c5bf3159
Aia Rupsom <aiale88@gmail.com>
2020-08-02 10:59:48 +0300

    add logger layer with bugsnag

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import logger from './app/utility/logger';
+
+logger.start();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
