d0f35a1af7353af33613c274b641b319772278d4
Aia Rupsom <aiale88@gmail.com>
2020-08-02 11:33:56 +0300

    add settings and colors config

---
+++ app/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+  black: '#000',
+  danger: '#ff5252',
+  dark: '#0c0c0c',
+  light: '#f8f4f4',
+  lightBlue: '#0d153a',
+  medium: '#6e6969',
+  primary: '#02062f',
+  secondary: '#6d3eaf',
+  white: '#fff',
+};
