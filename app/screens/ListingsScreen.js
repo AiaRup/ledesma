601c3e77977be5f37cf344f911250e9a02f12314
Aia Rupsom <aiale88@gmail.com>
2020-09-16 17:22:36 +0300

    start creating listings scrren with a form

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -12,7 +12,8 @@ import {
 import colors from '../config/colors';
 import routes from '../navigation/routes';
 import listingsApi from '../api/listings';
-import useApi from '../hooks/useApi';
+import farmsApi from '../api/farms';
+import headsApi from '../api/heads';
 
 export const ListingsScreen = ({ navigation }) => {
   // const { data: listings, error, loading, request: loadListings } = useApi(
@@ -60,6 +61,11 @@ export const ListingsScreen = ({ navigation }) => {
   const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState('date');
   const [show, setShow] = useState(false);
+  const [farms, setFarms] = useState([]);
+  const [heads, setHeads] = useState([]);
+  const [selectedHead, setSelectedHead] = useState(null);
+  const [selectedFarm, setSelectedFarm] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -74,6 +80,80 @@ export const ListingsScreen = ({ navigation }) => {
 
   const showDatepicker = () => {
     showMode('date');
+  };
+
+  const getFarmsList = async () => {
+    const farmsResult = await farmsApi.getFarms();
+    setFarms(farmsResult?.data?.docs || []);
+  };
+
+  const getHeadsList = async () => {
+    const headsResult = await headsApi.getHeads();
+    setHeads(headsResult?.data?.docs || []);
+  };
+
+  async function getData() {
+    setLoading(true);
+    await getFarmsList();
+    await getHeadsList();
+    setLoading(false);
+  }
+
+  const getLatestListing = async () => {
+    const listing =
+      (selectedHead &&
+        (await listingsApi.getLatestListing(selectedHead._id))) ||
+      {};
+    listing &&
+      listing.data &&
+      listing.data.docs.length &&
+      setLatestFlowmeter(listing.data.docs[0].flowmeter);
+  };
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getData();
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      await getLatestListing();
+      setLoading(false);
+    }
+    getData();
+  }, [selectedHead]);
+
+  const resetState = () => {
+    setLoading(false);
+    setSelectedHead(null);
+    setSelectedFarm(null);
+    setFarms([]);
+    setHeads([]);
+  };
+
+  const handleSubmit = async (listing, { resetForm }) => {
+    setProgress(0);
+    setUploadVisible(true);
+
+    const result = await listingsApi.addListing(
+      { ...listing, location },
+      (progress) => setProgress(progress)
+    );
+
+    if (!result.ok) {
+      setUploadVisible(false);
+      return alert('Error al guardar los datos.');
+    }
+
+    const active = !!listing.operation;
+    await headsApi.updateHeadStatus(selectedHead._id, { active });
+    resetForm();
+    resetState();
+    navigation.navigate(routes.LISTING_DETAILS, result.data);
   };
 
   return (
@@ -94,6 +174,45 @@ export const ListingsScreen = ({ navigation }) => {
           />
         )}
       </View>
+      <AppForm
+        initialValues={{
+          farm: '',
+          head: '',
+          operation: '',
+          pressurePump: '',
+          pressureField: '',
+          flowmeter: '',
+        }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <AppFormPicker
+          items={farms}
+          name='farm'
+          placeholder='Quinta'
+          icon='tractor'
+          onChange={(value) => {
+            setSelectedFarm(value);
+            setSelectedHead(null);
+            setOperation({});
+          }}
+          loading={loading}
+        />
+        <AppFormPicker
+          items={heads}
+          name='head'
+          placeholder='Filtrado'
+          icon='filter'
+          dependedField='farm'
+          onChange={(value) => setSelectedHead(value)}
+          dependedFunc={(dependedValue) =>
+            heads.filter((value) => value.farm._id === dependedValue)
+          }
+          disabled={!selectedFarm}
+          loading={loading}
+        />
+        <SubmitButton title='Enviar Datos' />
+      </AppForm>
     </Screen>
   );
 };
@@ -104,3 +223,144 @@ const styles = StyleSheet.create({
     backgroundColor: colors.light,
   },
 });
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, ScrollView } from 'react-native';
+import * as Yup from 'yup';
+
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+  Screen,
+} from '../components';
+import useLocation from '../hooks/useLocation';
+import farmsApi from '../api/farms';
+import headsApi from '../api/heads';
+import listingsApi from '../api/listings';
+import { UploadScreen } from './UploadScreen';
+import routes from '../navigation/routes';
+
+const validationSchema = Yup.object().shape({
+  farm: Yup.string().required('Campo Requerido.'),
+  head: Yup.string().required('Campo Requerido.'),
+  flowmeter: Yup.number()
+    .typeError('Caudalímetro tiene que ser un numero.')
+    .required('Campo Requerido.'),
+  pressurePump: Yup.number(),
+  pressureField: Yup.number(),
+});
+
+export const ListingEditScreen = ({ navigation }) => {
+  const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <Screen style={styles.container}>
+      <ScrollView>
+        <UploadScreen
+          onDone={() => setUploadVisible(false)}
+          progress={progress}
+          visible={uploadVisible}
+        />
+        <AppForm
+          initialValues={{
+            farm: '',
+            head: '',
+            operation: '',
+            pressurePump: '',
+            pressureField: '',
+            flowmeter: '',
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+        >
+          <AppFormPicker
+            items={farms}
+            name='farm'
+            placeholder='Quinta'
+            icon='tractor'
+            onChange={(value) => {
+              setSelectedFarm(value);
+              setSelectedHead(null);
+              setOperation({});
+            }}
+            loading={loading}
+          />
+          <AppFormPicker
+            items={heads}
+            name='head'
+            placeholder='Filtrado'
+            icon='filter'
+            dependedField='farm'
+            onChange={(value) => setSelectedHead(value)}
+            dependedFunc={(dependedValue) =>
+              heads.filter((value) => value.farm._id === dependedValue)
+            }
+            disabled={!selectedFarm}
+            loading={loading}
+          />
+          <AppFormPicker
+            items={heads}
+            name='operation'
+            placeholder='Operación'
+            icon='format-list-numbered'
+            dependedField='head'
+            onChange={(value) => setOperation(value)}
+            dependedFunc={(dependedValue) => {
+              const head = heads.filter((value) => value._id === dependedValue);
+              return head.length ? head[0].operations : [];
+            }}
+            disabled={!selectedHead}
+          />
+          <AppFormField
+            maxLength={255}
+            multiline
+            name='flowmeter'
+            placeholder='Caudalímetro'
+            icon='water-pump'
+            keyboardType='number-pad'
+            disabled={!!selectedHead}
+            validate={(value) => {
+              if (!value || !latestFlowmeter) {
+                return;
+              }
+              if (latestFlowmeter && value <= latestFlowmeter) {
+                return `El valor debe estar mas grande del ultimo caudalímetro registrado:  ${latestFlowmeter}`;
+              }
+            }}
+          />
+          <AppFormField
+            maxLength={255}
+            multiline
+            name='pressurePump'
+            keyboardType='number-pad'
+            placeholder='Presión - Bomba'
+            validate={(value) => {
+              if (value < operation.pump.min || value > operation.pump.max) {
+                return `El valor debe estar entre ${operation.pump.min} y ${operation.pump.max}`;
+              }
+            }}
+            disabled={!!selectedHead}
+          />
+          <AppFormField
+            maxLength={255}
+            multiline
+            name='pressureField'
+            keyboardType='number-pad'
+            placeholder='Presión - Campo'
+            validate={(value) => {
+              if (value < operation.field.min || value > operation.field.max) {
+                return `El valor debe estar entre ${operation.field.min} y ${operation.field.max}`;
+              }
+            }}
+            disabled={!!selectedHead}
+          />
+          <SubmitButton title='Enviar Datos' />
+        </AppForm>
+      </ScrollView>
+    </Screen>
+  );
+};
