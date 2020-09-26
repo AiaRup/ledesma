afd6841e529db8de96e7e86d773c2806ffa83cce
Aia Rupsom <aiale88@gmail.com>
2020-09-26 20:24:07 +0300

    fix error on app loading - add useEffect

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppLoading } from 'expo';
 import { I18nManager } from 'react-native';
@@ -21,26 +21,27 @@ export default function App() {
   const [user, setUser] = useState(null);
   const [isReady, setIsReady] = useState(false);
 
-  const restoreUser = async () => {
-    const user = await authStorage.getUser();
-    if (user) setUser(user);
-  };
-
-  if (!isReady)
-    return (
-      <AppLoading
-        startAsync={restoreUser}
-        onFinish={() => setIsReady(true)}
-        onError={() => logger.log('Error on app loading')}
-      />
-    );
+  useEffect(() => {
+    const restoreUser = async () => {
+      const user = await authStorage.getUser();
+      if (user) setUser(user);
+      setIsReady(true);
+    };
+    restoreUser();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      <OfflineNotice />
-      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
-        {user ? <AppNavigator /> : <AuthNavigator />}
-      </NavigationContainer>
-    </AuthContext.Provider>
+    <>
+      {!isReady ? (
+        <AppLoading onError={() => logger.log('Error on app loading')} />
+      ) : (
+        <AuthContext.Provider value={{ user, setUser }}>
+          <OfflineNotice />
+          <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+            {user ? <AppNavigator /> : <AuthNavigator />}
+          </NavigationContainer>
+        </AuthContext.Provider>
+      )}
+    </>
   );
 }
